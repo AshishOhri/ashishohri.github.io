e40c4ceb9ae8ce6b20ef4d9c504b5a4b007c06f0
Ashish Ohri <44030156+AshishOhri@users.noreply.github.com>
2024-06-23 21:38:07 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+(function($) {
+    "use strict";
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 70)
+                }, 1000, "easeInOutExpo");
+                return !1
+            }
+        }
+    });
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide')
+    });
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 100
+    });
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink")
+        } else {
+            $("#mainNav").removeClass("navbar-shrink")
+        }
+    };
+    navbarCollapse();
+    $(window).scroll(navbarCollapse)
+})(jQuery);
+var onMapMouseleaveHandler = function(event) {
+    var that = $(this);
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none")
+}
+var onMapClickHandler = function(event) {
+    var that = $(this);
+    that.off('click', onMapClickHandler);
+    that.find('iframe').css("pointer-events", "auto");
+    that.on('mouseleave', onMapMouseleaveHandler)
+}
+$('.map').on('click', onMapClickHandler);
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = !1
+};
+TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1)
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1)
+    }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    var that = this;
+    var delta = 100;
+    if (this.isDeleting) {
+        delta /= 2
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = !0
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = !1;
+        this.loopNum++;
+        delta = 500
+    }
+    setTimeout(function() {
+        that.tick()
+    }, delta)
+};
+window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period)
+        }
+    }
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css)
+}
